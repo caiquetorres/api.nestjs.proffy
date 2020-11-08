efa95b9010071f7be36d16cb4dd3406da486ffec
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-08 17:52:41 -0300

    feat(app.module.ts): added the new modules in the AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+
+import { AuthModule } from './modules/auth/auth.module'
+import { FavoriteModule } from './modules/favorite/favorite.module'
+import { SubjectModule } from './modules/subject/subject.module'
+import { TimeModule } from './modules/time/time.module'
+import { UserModule } from './modules/user/user.module'
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService]
+    imports: [UserModule, SubjectModule, TimeModule, FavoriteModule, AuthModule]
 })
 export class AppModule {}
