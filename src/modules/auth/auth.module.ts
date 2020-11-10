dd1a1287c2629a11d72c493c1c679ac8b9c82c65
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-09 21:25:41 -0300

    feat(auth.module.ts): added the strategies in auth module imports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common'
 import { AuthController } from './controllers/auth.controller'
 
 import { AuthService } from './services/auth.service'
+import { JwtStrategy } from './services/jwt.strategy.service'
+import { LocalStrategy } from './services/local.strategy.service'
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, JwtStrategy, LocalStrategy],
     exports: [AuthService]
 })
 export class AuthModule {}
