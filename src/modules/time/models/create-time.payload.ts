06483f2e96123cde039b2ede9e18155d252dc99a
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-09 21:21:40 -0300

    feat(create-time.payload.ts): finished the create-time payload class

---
+++ src/modules/time/models/create-time.payload.ts
@@ -0,0 +1,14 @@
+import { IsDefined, IsNumber } from 'class-validator'
+import { DefaultValidationMessages } from 'src/models/enums/default-validation-messages'
+
+export class CreateTimePayload {
+    @IsNumber({}, { message: DefaultValidationMessages.IsNumber })
+    @IsDefined({ message: 'É necessário informar o dia da semana.' })
+    public weekDay: number
+
+    @IsDefined({ message: 'É necessário informar a data inicial.' })
+    public from: Date
+
+    @IsDefined({ message: 'É necessário informar a data final.' })
+    public to: Date
+}
