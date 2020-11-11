fa6ba3a96cf40214abecd59fe70e72745d6779be
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-11 18:45:34 -0300

    feat(user.controller.ts & user.service.ts): added some logic that allows protecting some routes

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,8 +1,22 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, UseGuards } from '@nestjs/common'
+
+import { UserProxy } from '../models/user.proxy'
 
 import { UserService } from '../services/user.service'
 
-@Controller('user')
+import { User } from 'src/decorators/user/user.decorator'
+import { JwtAuthGuard } from 'src/guards/jwt.guard'
+import { RolesAuthGuard } from 'src/guards/roles.guard'
+import { RequestUser } from 'src/utils/type.shared'
+
+@Controller('users')
 export class UserController {
     public constructor(private readonly userService: UserService) {}
+
+    @UseGuards(JwtAuthGuard, RolesAuthGuard)
+    @Get('me')
+    public async getMe(@User() requestUser: RequestUser): Promise<UserProxy> {
+        const entity = await this.userService.get(requestUser.id, requestUser)
+        return entity.toProxy()
+    }
 }
--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common'
+import {
+    Injectable,
+    NotFoundException,
+    UnauthorizedException
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
+import { Repository } from 'typeorm'
+
+import { UserEntity } from '../entities/user.entity'
+
+import { CreateUserPayload } from '../models/create-user.payload'
+
+import { hasPermission } from 'src/utils/functions'
+import { RequestUser } from 'src/utils/type.shared'
 
 @Injectable()
-export class UserService {}
+export class UserService extends TypeOrmCrudService<UserEntity> {
+    public constructor(
+        @InjectRepository(UserEntity)
+        public repository: Repository<UserEntity>
+    ) {
+        super(repository)
+    }
+
+    public async create(
+        requestUser: RequestUser,
+        createUserPayload: CreateUserPayload
+    ): Promise<UserEntity> {
+        const entity = new UserEntity(createUserPayload)
+
+        return await entity.save()
+    }
+
+    public async get(
+        entityId: number,
+        requestUser: RequestUser
+    ): Promise<UserEntity> {
+        const entity = UserEntity.findOne(entityId)
+        if (!hasPermission(entityId, requestUser))
+            throw new UnauthorizedException(
+                'You have no permission to access those sources'
+            )
+
+        if (!entity)
+            throw new NotFoundException(
+                `The entity identified by "${entityId}" was not found`
+            )
+
+        return entity
+    }
+}
