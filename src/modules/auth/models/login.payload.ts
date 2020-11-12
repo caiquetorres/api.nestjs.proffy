bd89b88ee31ed1f357a7bfc749d789856d87cbc9
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-12 20:54:00 -0300

    feat(login.payload.ts): added some decorators in the properties to improve the file readability

--- src/modules/auth/models/login.payload.ts
+++ src/modules/auth/models/login.payload.ts
@@ -1,4 +1,14 @@
+import { IsDefined, IsString } from 'class-validator'
+import { DefaultValidationMessages } from 'src/models/enums/default-validation-messages.enum'
+
 export class LoginPayload {
-    email: string
-    password: string
+    @IsDefined({ message: 'It is necessary to inform the e-mail' })
+    @IsString({ message: DefaultValidationMessages.IsString })
+    public email: string
+
+    @IsDefined({
+        message: 'It is necessary to inform the password'
+    })
+    @IsString({ message: DefaultValidationMessages.IsString })
+    public password: string
 }
