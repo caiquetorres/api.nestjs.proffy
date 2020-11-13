5b343c72ea352837758da374c66a41230680099c
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-12 21:08:33 -0300

    style(auth.controller.ts): added some comments; reorganized the imports

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
 
+import { User } from 'src/decorators/user/user.decorator'
+
+import { LocalAuthGuard } from 'src/guards/local.guard'
+
 import { TokenProxy } from '../models/token.proxy'
 
 import { AuthService } from '../services/auth.service'
 
-import { User } from 'src/decorators/user/user.decorator'
-import { LocalAuthGuard } from 'src/guards/local.guard'
 import { RequestUser } from 'src/utils/type.shared'
 
 @Controller('auth')
 export class AuthController {
     public constructor(private readonly authService: AuthService) {}
 
+    /**
+     * Method that can return the token
+     * @param requestUser stores the user base data
+     */
     @UseGuards(LocalAuthGuard)
     @Post('/local')
     public async signIn(@User() user: RequestUser): Promise<TokenProxy> {
