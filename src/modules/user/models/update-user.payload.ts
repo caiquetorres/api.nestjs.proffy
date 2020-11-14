7d34f710a03ea5e7a4ebb138a4fae7456736560c
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-14 15:35:35 -0300

    feat(update-user.payloas.ts): added the last user properties

--- src/modules/user/models/update-user.payload.ts
+++ src/modules/user/models/update-user.payload.ts
@@ -1,5 +1,5 @@
 import { DefaultValidationMessages } from '../../../models/enums/default-validation-messages.enum'
-import { IsOptional, IsString } from 'class-validator'
+import { IsBase64, IsNumber, IsOptional, IsString } from 'class-validator'
 
 export class UpdateUserPayload {
     @IsOptional()
@@ -9,4 +9,20 @@ export class UpdateUserPayload {
     @IsOptional()
     @IsString({ message: DefaultValidationMessages.IsString })
     public lastName?: string
+
+    @IsOptional()
+    @IsBase64({ message: DefaultValidationMessages.IsBase64 })
+    public photo?: string
+
+    @IsOptional()
+    @IsString({ message: DefaultValidationMessages.IsString })
+    public whatsapp?: string
+
+    @IsOptional()
+    @IsString({ message: DefaultValidationMessages.IsString })
+    public description?: string
+
+    @IsOptional()
+    @IsNumber({}, { message: DefaultValidationMessages.IsNumber })
+    public price?: number
 }
