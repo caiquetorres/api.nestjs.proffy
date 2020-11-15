05b375d3b4b0d52860cdeed3aa7a8a077bdce4f8
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-15 15:57:02 -0300

    feat(time.proxy.ts): made all the properties in this class optionals

--- src/modules/time/models/time.proxy.ts
+++ src/modules/time/models/time.proxy.ts
@@ -2,18 +2,19 @@ import { TimeEntity } from '../entities/time.entity'
 
 import { UserProxy } from 'src/modules/user/models/user.proxy'
 
+// review
 export class TimeProxy {
-    public id: number
-    public weekDay: number
-    public from: Date
-    public to: Date
-    public user: UserProxy
+    public id?: number
+    public weekDay?: number
+    public from?: Date
+    public to?: Date
+    public user?: UserProxy
 
-    public constructor(entity: TimeEntity) {
-        this.id = entity.id
-        this.weekDay = entity.weekDay
-        this.from = entity.from
-        this.to = entity.to
-        this.user = entity.user.toProxy()
+    public constructor(partial: Partial<TimeEntity>) {
+        this.id = partial.id
+        this.weekDay = partial.weekDay
+        this.from = partial.from
+        this.to = partial.to
+        this.user = partial.user.toProxy()
     }
 }
