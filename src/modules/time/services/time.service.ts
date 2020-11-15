9b937406b83f03feea24e8941d2196285dbad03e
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-15 16:01:45 -0300

    feat(time.controller.ts & time.service.ts): finished the time CRUD

--- src/modules/time/services/time.service.ts
+++ src/modules/time/services/time.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import {
+    Injectable,
+    NotFoundException,
+    UnauthorizedException
+} from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
 import { Repository } from 'typeorm'
@@ -6,21 +10,29 @@ import { Repository } from 'typeorm'
 import { TimeEntity } from '../entities/time.entity'
 
 import { CreateTimePayload } from '../models/create-time.payload'
+import { UpdateTimePayload } from '../models/update-time.payload'
 
 import { UserService } from 'src/modules/user/services/user.service'
 
+import { hasPermission } from 'src/utils/functions'
 import { RequestUser } from 'src/utils/type.shared'
 
 @Injectable()
 export class TimeService extends TypeOrmCrudService<TimeEntity> {
     public constructor(
         @InjectRepository(TimeEntity)
-        private readonly repository: Repository<TimeEntity>,
+        repository: Repository<TimeEntity>,
         private readonly userService: UserService
     ) {
         super(repository)
     }
 
+    /**
+     * Method that can add a time entity in the database
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param createTimePayload stores the new time data
+     */
     public async create(
         requestUser: RequestUser,
         userId: number,
@@ -32,5 +44,75 @@ export class TimeService extends TypeOrmCrudService<TimeEntity> {
             user
         })
         return await entity.save()
+    }
+
+    /**
+     * Method that can return a specific time
+     * @param timeId stores the time id
+     */
+    public async get(timeId: number): Promise<TimeEntity> {
+        const entity = await TimeEntity.findOne({
+            where: { id: timeId },
+            relations: ['user']
+        })
+
+        if (!entity)
+            throw new NotFoundException(
+                `The entity identified by "${timeId}" was not found`
+            )
+
+        return entity
+    }
+
+    /**
+     * Method that can update the time entity data
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param timeId stores the time id
+     * @param updateTimePayload stores the new time data
+     */
+    public async update(
+        requestUser: RequestUser,
+        userId: number,
+        timeId: number,
+        updateTimePayload: UpdateTimePayload
+    ): Promise<void> {
+        if (!hasPermission(requestUser, userId))
+            throw new UnauthorizedException(
+                'You have no permission to access those sources'
+            )
+
+        const exists = await TimeEntity.exists(timeId)
+        if (!exists)
+            throw new NotFoundException(
+                `The entity identified by "${timeId}" was not found`
+            )
+
+        await TimeEntity.update({ id: timeId }, updateTimePayload)
+    }
+
+    /**
+     * Method that can remove a time entity from the database
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param timeId store the time id
+     */
+    public async delete(
+        requestUser: RequestUser,
+        userId: number,
+        timeId: number
+    ): Promise<void> {
+        if (!hasPermission(requestUser, userId))
+            throw new UnauthorizedException(
+                'You have no permission to access those sources'
+            )
+
+        const exists = await TimeEntity.exists(timeId)
+        if (!exists)
+            throw new NotFoundException(
+                `The entity identified by "${timeId}" was not found`
+            )
+
+        await TimeEntity.delete({ id: timeId })
     }
 }
