47307117be1b9780a7de7cda87bb96052e217a79
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-15 17:30:23 -0300

    feat(favorite.controller.ts & favorite.module.ts & favorite.service.ts): finished the basic favorite CRUD

--- src/modules/favorite/controllers/favorite.controller.ts
+++ src/modules/favorite/controllers/favorite.controller.ts
@@ -1,8 +1,93 @@
-import { Controller } from '@nestjs/common'
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    UseGuards
+} from '@nestjs/common'
+
+import { Roles } from 'src/decorators/roles/roles.decorator'
+import { User } from 'src/decorators/user/user.decorator'
+
+import { JwtAuthGuard } from 'src/guards/jwt/jwt.guard'
+import { RolesAuthGuard } from 'src/guards/roles/roles.guard'
+
+import { CreateFavoritePayload } from '../models/create-favorite.payload'
+import { FavoriteProxy } from '../models/favorite.proxy'
 
 import { FavoriteService } from '../services/favorite.service'
 
-@Controller('favorite')
+import { RequestUser } from 'src/utils/type.shared'
+
+import { RoleTypes } from 'src/models/enums/roles.enum'
+
+@Controller('users/:userId/favorites')
 export class FavoriteController {
     public constructor(private readonly favoriteService: FavoriteService) {}
+
+    /**
+     * Method that can add a favorite entity in the database
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param createFavoritePayload stores the new favorite entity data
+     */
+    @UseGuards(RolesAuthGuard)
+    @Roles(RoleTypes.ADMIN, RoleTypes.USER)
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    public async create(
+        @User() requestUser: RequestUser,
+        @Param('userId') userId: number,
+        @Body() createFavoritePayload: CreateFavoritePayload
+    ): Promise<FavoriteProxy> {
+        const entity = await this.favoriteService.create(
+            requestUser,
+            userId,
+            createFavoritePayload
+        )
+        return entity.toProxy()
+    }
+
+    /**
+     * Method that can return a favorite entity
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param favoriteId stores the favorite user id
+     */
+    @UseGuards(RolesAuthGuard)
+    @Roles(RoleTypes.ADMIN, RoleTypes.USER)
+    @UseGuards(JwtAuthGuard)
+    @Get(':id')
+    public async get(
+        @User() requestUser: RequestUser,
+        @Param('userId') userId: number,
+        @Param('id') favoriteId: number
+    ): Promise<FavoriteProxy> {
+        const entity = await this.favoriteService.get(
+            requestUser,
+            userId,
+            favoriteId
+        )
+        return entity.toProxy()
+    }
+
+    /**
+     * Method that remove a favorite entity from the database
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param favoriteId stores the favorite user id
+     */
+    @UseGuards(RolesAuthGuard)
+    @Roles(RoleTypes.ADMIN, RoleTypes.USER)
+    @UseGuards(JwtAuthGuard)
+    @Delete(':id')
+    public async delete(
+        @User() requestUser: RequestUser,
+        @Param('userId') userId: number,
+        @Param('id') favoriteId: number
+    ): Promise<void> {
+        await this.favoriteService.delete(requestUser, userId, favoriteId)
+    }
 }
--- src/modules/favorite/services/favorite.service.ts
+++ src/modules/favorite/services/favorite.service.ts
@@ -1,4 +1,111 @@
-import { Injectable } from '@nestjs/common'
+import {
+    Injectable,
+    NotFoundException,
+    UnauthorizedException
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
+import { Repository } from 'typeorm'
+
+import { FavoriteEntity } from '../entities/favorite.entity'
+
+import { CreateFavoritePayload } from '../models/create-favorite.payload'
+
+import { UserService } from 'src/modules/user/services/user.service'
+
+import { RequestUser } from 'src/utils/type.shared'
+import { hasPermission } from 'src/utils/validation'
 
 @Injectable()
-export class FavoriteService {}
+export class FavoriteService extends TypeOrmCrudService<FavoriteEntity> {
+    public constructor(
+        @InjectRepository(FavoriteEntity)
+        private readonly repository: Repository<FavoriteEntity>,
+        private readonly userService: UserService
+    ) {
+        super(repository)
+    }
+
+    /**
+     * Method that can add a favorite entity in the database
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param createFavoritePayload stores the new favorite entity data
+     */
+    public async create(
+        requestUser: RequestUser,
+        userId: number,
+        createFavoritePayload: CreateFavoritePayload
+    ): Promise<FavoriteEntity> {
+        if (!hasPermission(requestUser, userId))
+            throw new UnauthorizedException(
+                'You have no permission to access those sources'
+            )
+
+        const user = await this.userService.get(userId)
+        const favoriteUser = await this.userService.get(
+            createFavoritePayload.favoriteUserId
+        )
+
+        const entity = new FavoriteEntity({
+            user,
+            favoriteUser
+        })
+
+        return await entity.save()
+    }
+
+    /**
+     * Method that can return a favorite entity
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param favoriteId stores the favorite user id
+     */
+    public async get(
+        requestUser: RequestUser,
+        userId: number,
+        favoriteId: number
+    ): Promise<FavoriteEntity> {
+        if (!hasPermission(requestUser, userId))
+            throw new UnauthorizedException(
+                'You have no permission to access those sources'
+            )
+
+        const entity = await FavoriteEntity.findOne({
+            where: { id: favoriteId },
+            relations: ['user', 'favoriteUser']
+        })
+
+        if (!entity)
+            throw new NotFoundException(
+                `The entity identified by "${favoriteId}" was not found`
+            )
+
+        return entity
+    }
+
+    /**
+     * Method that remove a favorite entity from the database
+     * @param requestUser stores the user basic data
+     * @param userId stores the user id
+     * @param favoriteId stores the favorite user id
+     */
+    public async delete(
+        requestUser: RequestUser,
+        userId: number,
+        favoriteId: number
+    ): Promise<void> {
+        if (!hasPermission(requestUser, userId))
+            throw new UnauthorizedException(
+                'You have no permission to access those sources'
+            )
+
+        const exists = await FavoriteEntity.exists(favoriteId)
+        if (!exists)
+            throw new NotFoundException(
+                `The entity identified by "${favoriteId}" was not found`
+            )
+
+        await FavoriteEntity.delete({ id: favoriteId })
+    }
+}
