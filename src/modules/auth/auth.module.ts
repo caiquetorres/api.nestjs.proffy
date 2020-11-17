569354cdff1d5e843d23f90710b2bd1319151f16
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-16 22:19:48 -0300

    feat(auth.module.ts & .gitignore): set the .env file

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { UserModule } from '../user/user.module'
     imports: [
         forwardRef(() => UserModule),
         JwtModule.register({
-            secret: 'secret',
+            secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: '1h' }
         })
     ],
