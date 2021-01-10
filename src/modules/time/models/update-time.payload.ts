8a613e6f12bfaf103887358998bf1df1e0c239e6
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2021-01-10 19:08:43 -0300

    feat(create-time.payload.ts & update-time.payload.ts): implemented the new class validators decorators in those classes

--- src/modules/time/models/update-time.payload.ts
+++ src/modules/time/models/update-time.payload.ts
@@ -1,14 +1,20 @@
-import { IsNumber, IsOptional } from 'class-validator'
-import { DefaultValidationMessages } from 'src/models/classes/default-validation-messages'
+import { IsHour } from 'src/decorators/validators/is-hour.decorator'
+import { IsWeekDay } from 'src/decorators/validators/is-week-day.decorator'
+
+import { IsOptional } from 'class-validator'
 
 export class UpdateTimePayload {
     @IsOptional()
-    @IsNumber({}, { message: DefaultValidationMessages.isNumber })
+    @IsWeekDay({
+        message: 'It is required to send a valid week day'
+    })
     public weekDay?: number
 
     @IsOptional()
+    @IsHour({ message: 'It is required to inform a valid hour' })
     public from?: string
 
+    @IsHour({ message: 'It is required to inform a valid hour' })
     @IsOptional()
     public to?: string
 }
